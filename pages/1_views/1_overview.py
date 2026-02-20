a3241f5d524010d1dddeb12996f70fc1816f9a3a
educifuentes <edu.cifuentes@gmail.com>
2026-02-20 10:21:11 -0300

    style - overview

--- pages/1_views/1_overview.py
+++ pages/1_views/1_overview.py
@@ -11,8 +11,12 @@ from utilities.ui_components.icons import render_icon
 
 st.title(f"{render_icon('logo')} Horas")
 
+    
+
 # Load data
 df = fct_activities()
+
+st.markdown("Date range " + f"{df['date'].min().date()} - {df['date'].max().date()}")
 
 def display_dashboard_section(filtered_df, label):
     if not filtered_df.empty:
@@ -31,26 +35,28 @@ def display_dashboard_section(filtered_df, label):
         st.info(f"No hay actividades registradas para {label.lower()}.")
 
 # Reference date (Today)
-today = pd.Timestamp("2026-02-19")
+today = pd.Timestamp.now().normalize()
 
 # Layout: Today and This Week in columns
 col1, col2 = st.columns(2)
 
 with col1:
-    df_today = df[df["date"].dt.date == today.date()]
-    display_dashboard_section(df_today, "Hoy")
+    with st.container(height=600):
+        df_today = df[df["date"].dt.date == today.date()]
+        display_dashboard_section(df_today, "Hoy")
 
 with col2:
-    # Calculations for "This Week" (Monday to Sunday)
-    start_of_week = today - timedelta(days=today.weekday())
-    end_of_week = start_of_week + timedelta(days=6)
-    
-    df_week = df[(df["date"] >= start_of_week) & (df["date"] <= end_of_week)]
-    display_dashboard_section(df_week, "Esta Semana")
+    with st.container(height=600):
+        # Calculations for "This Week" (Monday to Sunday)
+        start_of_week = today - timedelta(days=today.weekday())
+        end_of_week = start_of_week + timedelta(days=6)
+        
+        df_week = df[(df["date"] >= start_of_week) & (df["date"] <= end_of_week)]
+        display_dashboard_section(df_week, "Semana")
 
 # Layout: This Month at the bottom
 st.divider()
-st.subheader("Resumen Mensual")
+st.subheader("Mes")
 # Calculations for "This Month"
 start_of_month = today.replace(day=1)
 # Simple way to get end of month for sorting/filtering
